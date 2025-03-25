d8da65cd0433651dbaa4ae95815e77d4b002849d
Josh Hermiston <197682310@qq.com>
2025-03-25 17:20:06 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -1,7 +1,7 @@
 
 import { connect } from 'cloudflare:sockets';
 
-let userID = '';
+let userID = 'bd1b7838-180a-436c-ad8c-4c8165bc3fd7';
 let proxyIP = '';
 //let sub = '';
 let subConverter = atob('U1VCQVBJLkNNTGl1c3Nzcy5uZXQ=');
